e3cba4e3cda9f1728b37df064b94c89fcd4c0f44
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2025-06-24 15:30:55 +0530

    Initial commit

---
+++ api/main.py
@@ -0,0 +1,12 @@
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/greet/{name}")
+def greet_user(name: str):
+    return {"greeting": f"Hello, {name}!"}
