d957e15d3c3d5c57f459dd58bd1ee2da68132cfd
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2025-06-25 11:05:23 +0530

    Create models

--- api/main.py
+++ api/main.py
@@ -1,12 +1,19 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+app.add_middleware (
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
-@app.get("/greet/{name}")
-def greet_user(name: str):
-    return {"greeting": f"Hello, {name}!"}
+@app.get("/")
+def health_check():
+    return  'Health Check Complete !'
+
+
